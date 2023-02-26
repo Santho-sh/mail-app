cf3a15d15705d75b8b0f97c7e0bc309e30ab10d0
Santhosh <santhoshkalish333@gmail.com>
2023-02-26 19:23:21 +0530

    view email

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -16,6 +16,7 @@ function compose_email() {
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
+  document.querySelector('#mail-view').style.display = 'none';
 
   // Clear out composition fields
   document.querySelector('#compose-recipients').value = '';
@@ -23,17 +24,20 @@ function compose_email() {
   document.querySelector('#compose-body').value = '';
 }
 
+
 function load_mailbox(mailbox) {
   
   // Show the mailbox and hide other views
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#mail-view').style.display = 'none';
 
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
   // Load and show all the mails
   let emailView = document.querySelector('#emails-view')
+
   fetch(`/emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
@@ -53,29 +57,33 @@ function load_mailbox(mailbox) {
 
         const mails = document.createElement('div');
         mails.classList.add('mail');
-        
-        const recipients = email.recipients;
-        const subject = email.subject;
-        const timestamp = email.timestamp;
-        
+        mails.setAttribute('data-id', email.id)
+
         const reci = document.createElement('p');
         reci.classList.add('mail-recipients');
-        reci.innerHTML = recipients;
+        reci.innerHTML = email.recipients;
         mails.appendChild(reci);
 
         const sub = document.createElement('p');
         sub.classList.add('mail-subject');
-        sub.innerHTML = subject;
+        sub.innerHTML = email.subject;
         mails.appendChild(sub);
 
         const time = document.createElement('p');
         time.classList.add('mail-time');
-        time.innerHTML = timestamp;
+        time.innerHTML = email.timestamp;
         mails.appendChild(time);
 
         emailView.appendChild(mails)
 
       })
+
+      document.querySelectorAll('.mail').forEach(function(mail) {
+        mail.addEventListener('click', function(event) {
+          const id = event.target.parentElement.dataset.id;
+          mail_view(id)
+        });
+      });
   });
 }
 
@@ -102,4 +110,42 @@ function send_email() {
     message.innerHTML =  result;
 
   });
+}
+
+function mail_view(id){
+
+  document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#compose-view').style.display = 'none';
+  document.querySelector('#mail-view').style.display = 'block';
+
+  const mail_view = document.querySelector('#mail-view');
+  mail_view.innerHTML = '';
+
+  const mail = document.createElement('div');
+  const from = document.createElement('p');
+  const to = document.createElement('p');
+  const subject = document.createElement('p');
+  const timestamp = document.createElement('p');
+  const body = document.createElement('p');
+
+  fetch(`/emails/${id}`)
+  .then(response => response.json())
+  .then(email => {
+      from.innerHTML = `From: ${email.sender}`;
+      to.innerHTML = `To: ${email.recipients}`;
+      subject.innerHTML = `Subject: ${email.subject}`;
+      timestamp.innerHTML = `Timestamp: ${email.timestamp}`;
+      body.innerHTML = email.body;
+
+      console.log(body)
+  });
+
+  mail.appendChild(from);
+  mail.appendChild(to);
+  mail.appendChild(subject);
+  mail.appendChild(timestamp);
+  mail.appendChild(body);
+
+  mail_view.appendChild(mail)
+
 }
